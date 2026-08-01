e5b1c08ffa8c679c3608c0233564c5c4940c9eaa
echoandecho <2315847048@qq.com>
2026-08-01 22:32:31 +0800

    Next.js支持静态构建

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',//告诉Next.js将应用程序导出为静态HTML文件（在out文件夹里），
+    // 而不是使用服务器端渲染（SSR）或增量静态生成（ISR）
+};
 
 export default nextConfig;
